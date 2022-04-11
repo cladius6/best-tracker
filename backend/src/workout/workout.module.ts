117b5b315dca9bc595429ad1f85bea55bacb95f9
Klaudiusz <d1f1.6@protonmail.com>
2022-04-11 22:46:53 +0200

    Workout stupid service added with controller.

--- backend/src/workout/workout.module.ts
+++ backend/src/workout/workout.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WorkoutController } from './workout.controller';
+import { WorkoutService } from './workout.service';
 
 @Module({
-  controllers: [WorkoutController]
+  controllers: [WorkoutController],
+  providers: [WorkoutService]
 })
 export class WorkoutModule {}
