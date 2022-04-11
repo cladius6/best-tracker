9e8f0e696e1d533ae935ad9577ebff0368f6e5b8
Klaudiusz <d1f1.6@protonmail.com>
2022-04-11 22:23:08 +0200

    To motivate my frontend.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-      <h1 className='text-3xl font-bold underline'>
-        Hello World!
-      </h1>
-  )
-}
+    <h1 className="text-3xl font-bold underline">
+      Retro Workout App Development Has Started!
+    </h1>
+  );
+};
 
-export default Home
+export default Home;
