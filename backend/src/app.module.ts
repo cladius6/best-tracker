0fca8551100ddca3f6a7313dd0bcf7ed5b8283a8
Klaudiusz <d1f1.6@protonmail.com>
2022-04-11 21:25:32 +0200

    Typeorm configured in nestjs app.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,18 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import path from 'path';
 import configuration from './config/configuration';
+import { TypeOrmConfigService } from './typeorm/typeorm.config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: path.resolve(__dirname, '../../.env.development'),
       load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      useClass: TypeOrmConfigService,
     }),
   ],
   controllers: [AppController],
