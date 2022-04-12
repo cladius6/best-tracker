842a719eb7c68df87dcf7eac0670c53a26d9af31
Klaudiusz <d1f1.6@protonmail.com>
2022-04-12 12:40:53 +0200

    Create workout endpoint added.

--- backend/src/workout/workout.controller.ts
+++ backend/src/workout/workout.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Put } from '@nestjs/common';
+import { WorkoutService } from './workout.service';
 
 @Controller('workout')
-export class WorkoutController {}
+export class WorkoutController {
+  constructor(private workoutService: WorkoutService) {}
+
+  @Put('')
+  async create(@Body() workout) {
+    return this.workoutService.create(workout);
+  }
+}
--- backend/src/workout/workout.service.ts
+++ backend/src/workout/workout.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { WorkoutEntity } from './entity/workout.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class WorkoutService {}
+export class WorkoutService {
+  constructor(
+    @InjectRepository(WorkoutEntity)
+    private workoutRepository: Repository<WorkoutEntity>,
+  ) {}
+
+  async create(workout: WorkoutEntity): Promise<WorkoutEntity> {
+    return await this.workoutRepository.save(workout);
+  }
+}
