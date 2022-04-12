a21d2e442b654c0189ad7b0e2bc3e497a14ec84d
Klaudiusz <d1f1.6@protonmail.com>
2022-04-12 09:41:06 +0200

    Workout stupid service added with controller.

--- backend/src/workout/interfaces/exercise.interface.ts
+++ backend/src/workout/interfaces/exercise.interface.ts
@@ -2,12 +2,9 @@ export interface IExercise {
   id: string;
   name: string;
   description: string;
-  image: string;
   category: string;
   muscles: string[];
   musclesSecondary: string[];
-  equipment: string[];
   level: string;
-  video: string;
   type: string;
 }
