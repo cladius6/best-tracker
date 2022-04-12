22683e99957e9c63ab720a64c4a712aa66d337fc
Klaudiusz <d1f1.6@protonmail.com>
2022-04-12 23:34:52 +0200

    CreateExerciseDto added.

---
+++ backend/src/exercise/dto/create-exercise.dto.ts
@@ -0,0 +1,34 @@
+import { IExercise } from '../interfaces/exercise.interface';
+import { Muscle } from '../enum/muscle.enum';
+import { IsEnum, IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateExerciseDto implements IExercise {
+  @IsInt()
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  category: string;
+
+  @IsEnum(Muscle)
+  muscle: Muscle;
+
+  @IsEnum(Muscle)
+  muscleSecondary: Muscle;
+
+  @IsNotEmpty()
+  @IsString()
+  level: string;
+
+  @IsNotEmpty()
+  @IsString()
+  type: string;
+}
