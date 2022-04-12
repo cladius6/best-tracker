750038daf96d74e407cf01b9f83e9ca384d5779c
Klaudiusz <d1f1.6@protonmail.com>
2022-04-12 11:00:06 +0200

    Exercises repository initialized.

--- backend/src/exercise/exercise.service.ts
+++ backend/src/exercise/exercise.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ExerciseEntity } from './entity/exercise.entity';
 
 @Injectable()
-export class ExerciseService {}
+export class ExerciseService {
+  constructor(
+    @InjectRepository(ExerciseEntity)
+    private exercisesRepository: Repository<ExerciseEntity>,
+  ) {}
+}
