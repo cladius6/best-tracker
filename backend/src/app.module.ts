f58c09bcee00ea5f2ab243ea7291214ecba37563
Klaudiusz <d1f1.6@protonmail.com>
2022-04-12 09:42:28 +0200

    Path import corrected.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import path from 'path';
+import * as path from 'path';
 import configuration from './config/configuration';
 import { TypeOrmConfigService } from './typeorm/typeorm.config.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
