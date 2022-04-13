968e2b1ef56bb898669e2a575bd0c1650a9ab3b4
Klaudiusz <d1f1.6@protonmail.com>
2022-04-13 20:37:31 +0200

    Main file uses app config.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfig } from './app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const appConfig = app.get(AppConfig);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(appConfig.port);
 }
 bootstrap();
