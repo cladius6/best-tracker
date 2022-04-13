c514a6c390ad144f52c01b0579f80ac81fad4a76
Klaudiusz <d1f1.6@protonmail.com>
2022-04-13 22:26:54 +0200

    CreateDTO id property validation removed

--- backend/src/exercise/dto/create-exercise.dto.ts
+++ backend/src/exercise/dto/create-exercise.dto.ts
@@ -1,11 +1,8 @@
 import { IExercise } from '../interfaces/exercise.interface';
 import { Muscle } from '../enum/muscle.enum';
-import { IsEnum, IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateExerciseDto implements IExercise {
-  @IsInt()
-  id: string;
-
   @IsNotEmpty()
   @IsString()
   name: string;
